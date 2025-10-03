0a5c0ae11332cd3eb0435f7325ea1860f437163b
EmmanuelRendon01 <emarendon1301@gmail.com>
2025-10-03 16:04:27 -0500

    feat: add user retrieval and creation methods in UserService with validation

--- Ticket/src/service/UserService.java
+++ Ticket/src/service/UserService.java
@@ -1,6 +1,7 @@
 package service;
 import dao.User.UserIMP;
 import domain.User;
+import util.Validator;
 
 import java.util.List;
 
@@ -14,8 +15,30 @@ public class UserService {
 
     public List<User> getAllUsers(){
         List<User> userList = userIMP.getAllUsers();
-        //Logic if we want.
+        //Logic if we need.
         return userList;
+    }
+
+    public User getUserByDni(String dni){
+        User user = userIMP.getUserByDni(dni);
+        //Logic if we need.
+        return user;
+    }
+
+    public void createUser(String name, String dni, String email, String rol){
+        //Validation logic
+
+        if (userIMP.getUserByDni(dni) == null){
+            //error message here
+        }else{
+            if (Validator.isValidName(name) || Validator.isInteger(dni) || Validator.isPositive(dni)
+                    || Validator.isValidEmail(email) || Validator.isValidName(rol)){
+                userIMP.createUser(name, dni, email, rol);
+            }
+        }
+
+
+
     }
 
 
