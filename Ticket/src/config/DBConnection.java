84a825dc909575e7cb4df80d96ac3d9d04a18ab7
EmmanuelRendon01 <emarendon1301@gmail.com>
2025-10-03 15:19:34 -0500

    feat: implement database connection management with properties loading

--- Ticket/src/config/DBConnection.java
+++ Ticket/src/config/DBConnection.java
@@ -1,4 +1,32 @@
 package config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBConnection {
+    private Properties loadProperties() {
+        Properties props = new Properties();
+        try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new RuntimeException("db.properties file not found in resources folder.");
+            }
+            props.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading db.properties", e);
+        }
+        return props;
+    }
+
+    public Connection getConnection() throws SQLException {
+        Properties props = loadProperties();
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
 }
